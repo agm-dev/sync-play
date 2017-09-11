cfcaaa59a4c0b81c5b875f21cf6f6e883acedd25
Adrian Gonzalo <gonzahh@gmail.com>
2017-09-12 00:26:55 +0200

    added code generation feature

--- sync-play.js
+++ sync-play.js
@@ -1,3 +1,30 @@
 'use strict';
 
 console.log('Init sync-play.js');
+
+const generateCode = () => uuidv1();
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  console.log('Content loaded');
+  const generateCodeBtn = document.querySelector('#generate-code');
+  const code = document.querySelector('#code');
+
+  // Load code if there is one saved:
+  if (typeof window.localStorage !== 'undefined' && typeof localStorage.getItem('sync-play-code') === 'string') {
+     code.value = localStorage.getItem('sync-play-code');
+     console.log('Loaded code from local storage');
+  }
+
+  // Generate code button:
+  generateCodeBtn.addEventListener('click', function() {
+    console.log('CLICK: Generate Code Button');
+    code.value = generateCode();
+    if (typeof window.localStorage !== 'undefined') {
+       localStorage.setItem('sync-play-code', code.value);
+       console.log('Saved code on local storage');
+    }
+    code.select();
+  }, false);
+
+}, true);
